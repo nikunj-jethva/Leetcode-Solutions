5a6398a7e0a7186f069491ba495f3e45d7ba90bd
nikunj-vs <jethvanikunj361@gmail.com>
2026-06-08 11:24:39 +0530

    Add brute force solution for Two Sum
    Implement brute force solution for Two Sum problem.

---
+++ 1-Two-Sum.java
@@ -0,0 +1,28 @@
+/*
+LeetCode #1 - Two Sum
+Difficulty: Easy
+
+Approach:
+Brute Force (Check every pair)
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
